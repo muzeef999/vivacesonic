733fdceff3cf0d0a1ec7a64ec9b278a2bc76d68a
jkmultiMediaInstition <jkmultimediainstitution@gmail.com>
2025-02-23 18:53:16 +0530

    13  commit

--- src/app/Compoents/Baners.jsx
+++ src/app/Compoents/Baners.jsx
@@ -6,25 +6,18 @@ import Form from './Form'
 const Baners = () => {
   return (
     <div className='home1 container-fluid'>
-      <div className='container-data container'>
+      <div className='container-data'>
         <div className='row'>
              <div className='d-flex  align-items-center col-md-6' style={{marginTop:'10%'}}>
                 <div style={{zIndex:9}}>
-                    <p className='banner0'>WELCOME TO <span className='banner4' style={{color:'red'}}>VIVACE SONICS</span>   </p>
+                    <p className='banner0'>WELCOME TO <span className='banner4' style={{color:'red'}}>VIVACE SONICs</span>   </p>
                     <p className='banner1'>We Design & Build Custom Solutions for Unique Challenges.</p>
                     <p className='banner2'>Our commitment to excellence drives us to prioritize quality, affordability, and timely project delivery. By consistently delivering value, we ensure customer satisfaction and build lasting trust through positive feedback.</p>
                 </div>
              </div>
              <div className='d-flex second-data col-md-6'>
-              <div className='bannerleadform' style={{zIndex:9}}>
-                <div className='ribbon'>
-                  <span>Feel free to talk to us.</span>
-                  <span className='fold bottom right'></span>
-                  <span className='fold bottom left'></span>
-                </div>
-                <div className='card'>
-                   <Form />
-                </div>
+              <div className='sideGif'>
+
               </div>
              </div>
         </div>
